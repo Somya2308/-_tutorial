d27d8541c1e53079ea6408c157f5f04187d5dd48
Somya Jain <47030644+Somya2308@users.noreply.github.com>
2021-05-01 16:11:41 +0530

    Add files via upload

---
+++ multiline.py
@@ -0,0 +1,40 @@
+Python 3.9.4 (v3.9.4:1f2e3088f3, Apr  4 2021, 12:32:44) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #this is an example for multiline output
+>>> 
+>>> # case 1 using """
+>>> x="""hey
+i
+am
+here"""
+>>> print(x)
+hey
+i
+am
+here
+>>> 
+>>> 
+>>> #case 2 using '''
+>>> y='''h
+e
+l
+l
+o
+w
+o
+r
+l
+d'''
+>>> print(y)
+h
+e
+l
+l
+o
+w
+o
+r
+l
+d
+>>> 
